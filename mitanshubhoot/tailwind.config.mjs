58da3db88be34f4e631b5fc63ff6c4bed7037f23
mitanshubhoot <55936322+mitanshubhoot@users.noreply.github.com>
2025-03-25 18:34:25 -0400

    Update tailwind.config.mjs

--- mitanshubhoot/tailwind.config.mjs
+++ mitanshubhoot/tailwind.config.mjs
@@ -1,29 +1,30 @@
+// tailwind.config.mjs
+
+import typography from '@tailwindcss/typography'
+import flowbite from 'flowbite/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		
-	],
-	darkMode: "class",
-	theme: {
-		fontFamily: {
-			'mono': ['Space Mono', 'monospace']
-		},
-		extend: {
-			colors: {
-				bgColor: "var(--theme-bg)",
-				textColor: "var(--theme-text)",
-				link: "var(--theme-link)",
-				accent: "var(--theme-accent)",
-				"accent-2": "var(--theme-accent-2)",
-				surface: "var(--theme-surface)",
-				quote: "var(--theme-quote)",
-				highlight: "var(--theme-highlight)"
-			},
-		}
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('flowbite/plugin')
-	],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      mono: ['Space Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        bgColor: 'var(--theme-bg)',
+        textColor: 'var(--theme-text)',
+        link: 'var(--theme-link)',
+        accent: 'var(--theme-accent)',
+        'accent-2': 'var(--theme-accent-2)',
+        surface: 'var(--theme-surface)',
+        quote: 'var(--theme-quote)',
+        highlight: 'var(--theme-highlight)',
+      },
+    },
+  },
+  plugins: [typography, flowbite],
 }
